56daf975d1b68d391cdbbcae58263e4d64df0df2
SudhakarH <sudhakarh1999@gmail.com>
2020-09-12 21:29:26 +0530

    Initial Commit

---
+++ age calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  age calculator
+//
+//  Created by sudhakar H on 12/09/20.
+//  Copyright © 2020 sudhakar H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
